62d6c712aeec590b64534dc7105e7a96d66b1aa2
carlospantin <carlospantin@live.com.mx>
2024-11-01 18:57:41 +0200

    US3

--- piglatin.py
+++ piglatin.py
@@ -1,13 +1,21 @@
 class PigLatin:
 
 
-    class PigLatin:
-        def __init__(self, phrase: str):
-            self._phrase = phrase
+    def __init__(self, phrase: str):
+        self._phrase = phrase
 
-        def get_phrase(self) -> str:
-            return self._phrase
+    def get_phrase(self) -> str:
+        return self._phrase
 
-        def translate(self) -> str:
-            if self._phrase == "":
-                return "nil"
+    def translate(self) -> str:
+        if self._phrase == "":
+            return "nil"
+
+        word = self._phrase
+        if word[0].lower() in "aeiou":
+            if word[-1].lower() == "y":
+                return word + "nay"
+            elif word[-1].lower() in "aeiou":
+                return word + "yay"
+            else:
+                return word + "ay"
